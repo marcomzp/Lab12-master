225c77f8492a018a9dab5a9eaa7b3b595ea566e4
KorrineG <Korrine2@illinois.edu>
2018-04-23 18:39:24 -0500

    Initial commit

---
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/updated.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs.cs125.lab12;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class updated extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.updated);
+    }
+}
